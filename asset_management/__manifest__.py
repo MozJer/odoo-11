8c46e1bfbe3a524e535c9b6c9e65e1c1f0558d83
raghadz <38248578+raghadz@users.noreply.github.com>
2018-04-15 11:14:13 +0300

    Revert "asset_mangement"
    This reverts commit adda480ad62ba33cce508ff1b28b3ac3111dea63.

--- asset_management/__manifest__.py
+++ asset_management/__manifest__.py
@@ -20,10 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base',
-               'account' ,
-                'asset_management'
-                ],
+    'depends': ['base'],
 
     # always loaded
     'data': [
