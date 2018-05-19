68a6ec833f61c012081b2fc5aa27a8f791e127bc
mhdanas97 <37138131+mhdanas97@users.noreply.github.com>
2018-05-19 14:14:24 +0300

    Add new View

--- asset_management/__manifest__.py
+++ asset_management/__manifest__.py
@@ -29,6 +29,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+		'views/location_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
